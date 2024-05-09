ee86feafee609cea13831fe87b1ee759005ed7a8
levywanke <levywanke@gmail.com>
2024-05-09 15:47:05 +0300

    corrected

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -12,7 +12,7 @@ def top_ten(subreddit):
     '''
     user = {'User-Agent': 'Lizzie'}
     url = requests.get('https://www.reddit.com/r/{}/hot/.json?limit=10'
-                       .format(subreddit), headers = user).json()
+                       .format(subreddit), headers=user).json()
     try:
         for post in url.get('data').get('children'):
             print(post.get('data').get('title'))
